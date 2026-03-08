28ce724096414ebcdd78570d900b4dfb16c21d27
Mirsaid Seyidli <seyidli.mirsaid@gmail.com>
2026-03-08 21:49:25 +0100

    Add files via upload
    https://www.youtube.com/watch?v=VSQ5fCgX1-4&list=PL8uXoSVYVb8TalGgO6vWd2dfyPkuNJC83&index=42

---
+++ Program(HM).cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+
+namespace Ref_OutConstructor_Static_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First task
+            /*
+                Bir metod yazın ki, iki tam ədədi qəbul etsin və onların yerini dəyişsin.
+                Bu metodda ref açar sözündən istifadə edin. Əsas Main metodunda iki ədəd verin və
+                metoddan sonra onların dəyişdiyindən əmin olun.
+            */
+
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            ChangeVaraible(firstNumber, secondNumber);
+            #endregion
+
+            #region Second task
+            /*
+                Student adında bir class yaradın. Bu class-da name, surname, və age sahələri olsun.Student üçün 
+                bir constructor yazın ki, bu 3 sahəni qəbul edib təyin etsin.
+            */
+
+            //var name = Console.ReadLine();
+            //var surname = Console.ReadLine();
+            //var age = int.Parse(Console.ReadLine());
+            //Student newStudent = new Student(name,surname,age);
+            #endregion
+
+
+            #region Third task
+
+            /*
+                Car adında bir class yaradın. Bu class-da static bir sahə(count) olsun və hər dəfə yeni obyekt
+                yaradıldıqda bu say artırılsın.Əlavə olaraq Car constructor-da bu artımı təmin edin
+            */
+
+            Car Bmw = new Car("F10","Sedan",260);
+            Car Mercedec = new Car("CLS","Sedan",240);
+
+            #endregion
+        }
+
+        static void ChangeVaraible(int first, int second)
+        {
+            int number = first;
+            first = second;
+            Console.WriteLine($"Birinci deyer {first} - ");
+            second = number;
+            Console.WriteLine($"Ikinci deyer {second} - ");
+        }
+    }
+}
+
+
+class Student
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+
+    public Student() { }
+
+    public Student(string name_, string surname_, int age_)
+    {
+        this.Name = name_;
+        this.Surname = surname_;
+        this.Age = age_;
+        //Console.WriteLine($"Adi: {Name} Soyadi: {Surname} Yasi: {Age}");
+    }
+}
+
+class Car
+{
+    public static int Count { get; set; } = 0;
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public double Speed { get; set; }
+    public Car() { }
+
+    public Car(string name_ , string type_, double speed_)
+    {
+        this.Name= name_;
+        this.Type = type_;
+        this.Speed = speed_;
+        Count++;
+        Console.WriteLine(Name);
+        Console.WriteLine(Type);
+        Console.WriteLine(Speed);
+        Console.WriteLine(Count);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Ref_OutConstructor_Static_05._22._25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+            Bank KapitalBank = new Bank("Birbank", 96000);
+
+            KapitalBank.HeadBranch = "Nerimanov Filiali";
+            //Console.WriteLine("Name: " + KapitalBank.Name + "HeadBranch: " +
+            //    KapitalBank.HeadBranch + "InitialCount: " + KapitalBank.InitialCount);
+
+            Bank PashaBank = new Bank("Pasha", 74000);
+
+            PashaBank.HeadBranch = "Port Baku Filiali";
+            //Console.WriteLine("Name: " + PashaBank.Name + "HeadBranch: " +
+            //    PashaBank.HeadBranch + "InitialCount: " + PashaBank.InitialCount);
+
+            #endregion
+
+            #region
+
+            Citizen first = new Citizen("Elnare");
+
+            Console.WriteLine(Citizen.CitizenCount);
+            Console.WriteLine(first.CitizenCountNonStatic);
+            Console.WriteLine(first.Name);
+
+            Citizen second = new Citizen("Seyid");
+
+            Console.WriteLine(Citizen.CitizenCount); // 
+            Console.WriteLine(second.CitizenCountNonStatic);
+            Console.WriteLine(second.Name);
+
+            Citizen third = new Citizen("Sara");
+
+            Console.WriteLine(Citizen.CitizenCount);
+            Console.WriteLine(third.CitizenCountNonStatic);
+            Console.WriteLine(third.Name);
+
+            Sun.Degree(270);
+            #endregion
+
+            int result = 3;
+            int noResult;
+            // out baslangicda deyer teleb etmir 
+            // ref ise baslangicda deyer teleb edir.
+            intTryParse(out noResult, "1", ref result);
+            //Console.WriteLine(result + " out olmayan deyer");
+            //Console.WriteLine(noResult + " out olan deyer");
+
+        }
+        static void intTryParse(out int outValue, string value, ref int noResult_)
+        {
+            outValue = 7;
+            noResult_ = 8;
+        }
+    }
+}
+
+
+class Bank
+{
+    public Bank() { }
+    public Bank(string name, int initialCount)
+    {
+        this.Name = name;
+        this.InitialCount = initialCount;
+    }
+    public Bank(int initialCount)
+    {
+        this.InitialCount = initialCount;
+    }
+
+    public static int BankCount { get; set; } = 0;
+    public string HeadBranch { get; set; }
+    public string Name { get; set; }
+    public int InitialCount { get; set; }
+    public bool hasAny { get; set; }
+}
+class Citizen
+{
+    public static int CitizenCount { get; set; } = 0;
+    public int CitizenCountNonStatic { get; set; } = 0;
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string CitizenShip { get; set; }
+    public string Gender { get; set; }
+    
+    public Citizen() { }
+    public Citizen(string name)
+    {
+        this.Name = name;
+        CitizenCount++;
+        CitizenCountNonStatic++;
+    }
+
+}
+static class Sun
+{
+    public static int Heat { get; set; }
+    public static void Degree(decimal num)
+    {
+        Console.WriteLine("Derece yuksekdir");
+    }
+    static Sun() { }
+}
